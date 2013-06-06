983dafb66f9e93fb44fc2217a5aa13facb32cd60
lewisgoddard <lewisgoddard@eustasy.co.uk>
2013-06-06 21:51:35 +0100

    Change About page content

--- about/index.php
+++ about/index.php
@@ -6,27 +6,42 @@
 
 
 
-<div class="section group">
-	<div class="col span_1_of_1">
-		<div class="bubble standard">
-			<h2>About</h2>
-		</div>
-	</div>
-</div>
-
-
+<h2>About</h2>
 
 <div class="section group">
-	<div class="col span_3_of_5">
-		<div class="bubble information">
-			<h3><img src="http://midoribrowser.org/images/action-love.png" class="sixtyfour floatleft"> Already installed!</h3>
-			<h5>Midori comes pre-installed as the default browser in elementary os, you just need to click the icon down the bottom, or search for it in applications.</h5>
+	<div class="col span_1_of_2">
+		<div class="bubble standard">
+			<h3>The Browser</h3>
+			<p><a href="http://midoribrowser.org/">Midori</a> is a lightweight, fast, and free web browser. It aligns well with the Xfce philosophy of making the most out of available resources. It is also the default browser of the beautiful <a href="http://elementaryos.org/">elementary os</a>.</p>
+			<div class="section group">
+				<div class="col span_1_of_2">
+					<ul>
+						<li>Fast</li>
+						<li>Lightweight</li>
+						<li>Open-Source</li>
+					</ul>
+				</div>
+				<div class="col span_1_of_2">
+					<ul>
+						<li>GTK 2/3	</li>
+						<li>WebKit</li>
+						<li>Granite</li>
+					</ul>
+				</div>
+			</div>
+			<h4>The License</h4>
+			<p>Midori is <a title="Midori PPA" href="https://launchpad.net/~midori/+archive/ppa">release</a> under the <a rel="license" href="http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html">GNU Lesser General Public License, version 2.1</a>, which makes it suitable for inclusion in both proprietary and free, open-source programs.</p>
 		</div>
 	</div>
-	<div class="col span_2_of_5">
+	<div class="col span_1_of_2">
 		<div class="bubble standard">
-			<h4><img src="http://midoribrowser.org/images/feature-help.png" class="sixtyfour floatleft"> Help</h4>
-			<p>If you need to find out how to do something, you can check our <a href="http://midoribrowser.org/faq/">Frequently Asked Questions</a>, or try the <a href="https://plus.google.com/communities/104613975513761463450">Google Plus Community</a>.</p>
+			<h3>The Developers</h3>
+			<div class="clear"><br></div>
+			<h4>Christian Dywan</h4>
+			<p>Christian likes to cook and eat Japanese. Add to that a passion for Chinese and Japanese tea. Christian enjoys hacking on Midori, working with elementary projects and QML. His development machine is an elementary Xfce hybrid based on Ubuntu.</p>
+			<div class="clear"><br></div>
+			<h4>Nancy Runge</h4>
+			<p>Nancy likes to cook and eat Japanese as well. She is a voluntary cat petter in a local animal shelter.</p>
 		</div>
 	</div>
 </div>
