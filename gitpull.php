20540a2b60fb73c28d5962061b73a2047e4caf5c
lewisgoddard <lewisgoddard@eustasy.co.uk>
2013-06-21 16:28:38 +0100

    Simplified GITPULL

--- gitpull.php
+++ gitpull.php
@@ -32,26 +32,15 @@ if ( $auth === 'glaflzbhlaczddmuxurmlujmdqtgvoqzzbpljshtfrohsqzfbtmazkgnkmudqaiq
 <link rel="icon" type="image/x-icon" href="http://www.eustasy.co.uk/favicon.ico">
 <link rel="shortcut icon" type="image/x-icon" href="http://www.eustasy.co.uk/favicon.ico">
 <link rel="apple shortcut icon" href="http://www.eustasy.co.uk/wp-content/themes/eustasy-three/200.png">
-<link href='http://fonts.googleapis.com/css?family=Raleway:200,300,400' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:400' rel='stylesheet' type='text/css'>
 <!--[if IE ]><style type="text/css">body { text-align: left !important; }</style><![endif]-->
 <!--[if IE 8]><style type="text/css">body { font-style: normal !important; }</style><![endif]-->
 <style type="text/css">
-body{background:#fafafa;margin:0px;padding:0px;text-align:center;top:0px;color:#333333;font:400 1em/1.4 'Raleway',Sans;min-width:750px;}
-#small-container{margin:5% auto 3%;padding:30px 0;width:100%;background:#ffffff;}
-h1{font:200 112px/1 'Raleway',Sans;padding:65px 	0px 0px 0px;text-align:center;display:block;margin:0px;opacity:0.7;
-transition:all 500ms ease-in-out;-o-transition:all 500ms ease-in-out;-ms-transition:all 500ms ease-in-out;-moz-transition:all 500ms ease-in-out;-webkit-transition:all 500ms ease-in-out;}
-h1:hover{opacity:1;
-transition:all 500ms ease-in-out;-o-transition:all 500ms ease-in-out;-ms-transition:all 500ms ease-in-out;-moz-transition:all 500ms ease-in-out;-webkit-transition:all 500ms ease-in-out;}
-h1 img{vertical-align:middle;height:100px;width:100px;}
-h1 a,h1 a:hover,h1 a:active{color:#333333;}
-a{color:#006AC0;text-decoration:none;
-transition:alll 500ms ease-in-out;-o-transition:alll 500ms ease-in-out;-ms-transition:alll 500ms ease-in-out;-moz-transition:alll 500ms ease-in-out;-webkit-transition:alll 500ms ease-in-out;}
-a:hover{color:#19B6EE;
-transition:alll 500ms ease-in-out;-o-transition:alll 500ms ease-in-out;-ms-transition:alll 500ms ease-in-out;-moz-transition:alll 500ms ease-in-out;-webkit-transition:alll 500ms ease-in-out;}
+body{background:#fafafa;margin:0;padding:0;text-align:center;color:#333333;font:400 1em/1.4 'Raleway',Sans;min-width:750px;}
+#small-container{margin:15% auto 12%;padding:5% 0 4%;width:100%;background:#ffffff;}
 </style>
 </head>
 <body>
-<h1><a href="http://www.eustasy.co.uk/"><img src="http://www.eustasy.co.uk/wp-content/themes/eustasy-three/200.png" alt="eustasy logo"> eustasy</a></h1>
 <div id="small-container">
 <pre><?php echo $output; // Kind of. ?></pre>
 </div>
