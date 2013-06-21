1d953be7a3e977073a0fed3a94866c316edd840d
lewisgoddard <lewisgoddard@eustasy.co.uk>
2013-06-21 14:13:49 +0100

    Activated GitPull

--- gitpull.php
+++ gitpull.php
@@ -1,11 +1,30 @@
 <?php
-$auth = htmlentities($_GET['auth'], ENT_QUOTES, "UTF-8");
+
+// This is the GitPull file. The correct URL to active it is:
 // http://midoribrowser.org/gitpull.php?auth=glaflzbhlaczddmuxurmlujmdqtgvoqzzbpljshtfrohsqzfbtmazkgnkmudqaiq
-if ( $auth === 'glaflzbhlaczddmuxurmlujmdqtgvoqzzbpljshtfrohsqzfbtmazkgnkmudqaiq' ) {
-	$output = nl2br( shell_exec('apt-cache search git pull') ) . '<br><br><br><p>GitHub Update Complete</p>';
-} else {
-	$output = 'Ha, nope.';
+
+$auth = htmlentities($_GET['auth'], ENT_QUOTES, "UTF-8"); // Encode auth before using it.
+
+// We can also check for payload to help stop attacks.
+// && isset($_POST['payload'])
+
+if ( $auth === 'glaflzbhlaczddmuxurmlujmdqtgvoqzzbpljshtfrohsqzfbtmazkgnkmudqaiq' ) { // If the auth is correct and Git posted a payload.
+
+	// We're going to run a few commands now.
+	// git reset --hard HEAD	// Cleans out uncommitted changes.
+	// git clean -f -d			// Remove junk files. DO NOT EXECUTE. DELETES DOWNLOADS.
+	// git pull					// Get the latest version.
+
+	// git reset --hard HEAD && git pull
+
+	$output = nl2br( shell_exec('git reset --hard HEAD && git pull') ); // Capture the output.
+
+} else { // Or not.
+	$output = 'Ha, nope.'; // Sassy.
 }
+
+// That is all.
+
 ?><!DOCTYPE HTML>
 <html>
 <head>
@@ -34,7 +53,7 @@ transition:alll 500ms ease-in-out;-o-transition:alll 500ms ease-in-out;-ms-trans
 <body>
 <h1><a href="http://www.eustasy.co.uk/definition/"><img src="http://www.eustasy.co.uk/wp-content/themes/eustasy-three/200.png">eustasy</a></h1>
 <div id="small-container">
-<pre><?php echo $output; ?></pre>
+<pre><?php echo $output; // Kind of. ?></pre>
 </div>
 </body>
 </html>
