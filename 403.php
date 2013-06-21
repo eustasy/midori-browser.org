3e337bdd4b3b186f4a0bccfa4f7cf72dc2071c09
lewisgoddard <lewisgoddard@eustasy.co.uk>
2013-06-21 16:06:59 +0100

    Added 403 Unauthorized page

--- 403.php
+++ 403.php
@@ -1,10 +1,10 @@
 <?php include 'chunks/doctype.php'; ?>
 
-	<title>Page not found &nbsp;&middot;&nbsp; Midori &nbsp;&middot;&nbsp; A lightweight, fast, and free web browser.</title>
+	<title>Unauthorized Access Terminated &nbsp;&middot;&nbsp; Midori &nbsp;&middot;&nbsp; A lightweight, fast, and free web browser.</title>
 
 	<?php include 'chunks/header.php'; ?>
 
-<h2><span style="color:#999">403 Error:</span> Unauthorized.</h2>
+<h2><span style="color:#999">403 Error:</span> Unauthorized Access Terminated.</h2>
 <h3>Sorry about that, here's a picture of a kitten!</h3>
 <div class="clear"><br></div>
 <a title="By Nicolas Suzor from Brisbane, Australia (Kittens!  Uploaded by Kaldari) [CC-BY-SA-2.0 (http://creativecommons.org/licenses/by-sa/2.0)], via Wikimedia Commons" href="http://commons.wikimedia.org/wiki/File%3ACute_grey_kitten.jpg"><img alt="Cute grey kitten" class="rounded" src="http://upload.wikimedia.org/wikipedia/commons/7/75/Cute_grey_kitten.jpg"/></a>
