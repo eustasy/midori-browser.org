5f80a869d979d26a8dedbb65062ad6f77f16410c
lewisgoddard <lewisgoddard@eustasy.co.uk>
2013-06-22 20:52:26 +0100

    Add currency classes

--- chunks/currency.js
+++ chunks/currency.js
@@ -2,18 +2,24 @@ window.onload = function(){
 
 var country = geoip_country_code();
 
-if (country === "EU" || country === "AD" || country === "AT" || country === "BE" || country === "CY" || country === "EE" || country === "FI" || country === "FR" || country === "DE" || country === "GR" || country === "IE" || country === "IT" || country === "XK" || country === "LU" || country === "MT" || country === "MC" || country === "ME" || country === "NL" || country === "PT" || country === "SM" || country === "SK" || country === "SI" || country === "ES" || country === "VA") {
-    $('#donationlocation').html('<input type=\"hidden\"name=\"lc\"value=\"US\"><input type=\"hidden\"name=\"currency_code\"value=\"EUR\">'); // EUR
-} else if (country === "GG" || country === "IM" || country === "JE" || country === "GS" || country === "GB") {
-    $('#donationlocation').html('<input type=\"hidden\"name=\"lc\"value=\"GB\"><input type=\"hidden\"name=\"currency_code\"value=\"GBP\">'); // GBP
-} else if (country === "AU" || country === "CC" || country === "KI" || country === "NR" || country === "TV") {
-    $('#donationlocation').html('<input type=\"hidden\"name=\"lc\"value=\"AU\"><input type=\"hidden\"name=\"currency_code\"value=\"AUD\">'); // AUD
-} else if (country === "CA") {
-    $('#donationlocation').html('<input type=\"hidden\"name=\"lc\"value=\"CA\"><input type=\"hidden\"name=\"currency_code\"value=\"CAD\">'); // CAD
-} else if (country === "SE") {
-    $('#donationlocation').html('<input type=\"hidden\"name=\"lc\"value=\"SE\"><input type=\"hidden\"name=\"currency_code\"value=\"SEK\">'); // CAD
-} else {
-    $('#donationlocation').html('<input type=\"hidden\"name=\"lc\"value=\"US\"><input type=\"hidden\"name=\"currency_code\"value=\"USD\">'); // USD
+if (country === "EU" || country === "AD" || country === "AT" || country === "BE" || country === "CY" || country === "EE" || country === "FI" || country === "FR" || country === "DE" || country === "GR" || country === "IE" || country === "IT" || country === "XK" || country === "LU" || country === "MT" || country === "MC" || country === "ME" || country === "NL" || country === "PT" || country === "SM" || country === "SK" || country === "SI" || country === "ES" || country === "VA") { // EUR
+    $('.donationlocation').html('<input type=\"hidden\"name=\"lc\"value=\"US\"><input type=\"hidden\"name=\"currency_code\"value=\"EUR\">');
+	$(".donate form").addClass("eur");
+} else if (country === "GG" || country === "IM" || country === "JE" || country === "GS" || country === "GB") { // GBP
+    $('.donationlocation').html('<input type=\"hidden\"name=\"lc\"value=\"GB\"><input type=\"hidden\"name=\"currency_code\"value=\"GBP\">');
+	$(".donate form").addClass("gbp");
+} else if (country === "AU" || country === "CC" || country === "KI" || country === "NR" || country === "TV") { // AUD
+    $('.donationlocation').html('<input type=\"hidden\"name=\"lc\"value=\"AU\"><input type=\"hidden\"name=\"currency_code\"value=\"AUD\">');
+	$(".donate form").addClass("aud");
+} else if (country === "CA") { // CAD
+    $('.donationlocation').html('<input type=\"hidden\"name=\"lc\"value=\"CA\"><input type=\"hidden\"name=\"currency_code\"value=\"CAD\">');
+	$(".donate form").addClass("cad");
+} else if (country === "SE") { // CAD
+    $('.donationlocation').html('<input type=\"hidden\"name=\"lc\"value=\"SE\"><input type=\"hidden\"name=\"currency_code\"value=\"SEK\">');
+	$(".donate form").addClass("sek");
+} else { // USD
+    $('.donationlocation').html('<input type=\"hidden\"name=\"lc\"value=\"US\"><input type=\"hidden\"name=\"currency_code\"value=\"USD\">');
+	$(".donate form").addClass("usd");
 }
 
 
