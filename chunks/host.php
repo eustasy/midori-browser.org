b1cf9a7d013bb74520822e2229ede73972f526bb
lewisgoddard <lewisgoddard@eustasy.co.uk>
2013-06-23 14:11:16 +0100

    Fixed location ariable

--- chunks/host.php
+++ chunks/host.php
@@ -4,9 +4,11 @@ $host = htmlspecialchars($_SERVER['HTTP_HOST'], ENT_QUOTES | ENT_HTML5, "UTF-8")
 
 if ( $host === '192.168.1.6' ) {
 	$home = 'http://192.168.1.6/midoribrowser.org';
+	$location = htmlspecialchars($_SERVER['REQUEST_URI'], ENT_QUOTES | ENT_HTML5, "UTF-8");
 	$location = str_replace('/midoribrowser.org', '', $location);
 } else {
 	$home = 'http://' . $host;
+	$location = htmlspecialchars($_SERVER['REQUEST_URI'], ENT_QUOTES | ENT_HTML5, "UTF-8");
 }
 
 ?>
