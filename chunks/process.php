91f21d31007a942a6fb2edc90142af787529c47e
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-08-15 20:13:07 +0100

    Removed Process reliance on Host

--- chunks/process.php
+++ chunks/process.php
@@ -1,8 +1,14 @@
 <?php
 
-$url = $home . '/chunks/downloads.php';
+$xmlhost = htmlspecialchars($_SERVER['HTTP_HOST'], ENT_QUOTES | ENT_HTML5, "UTF-8");
 
-$xml = simplexml_load_file($url);
+if ( $xmlhost === '192.168.1.6' ) {
+	$xmlurl = 'http://192.168.1.6/midoribrowser.org/chunks/downloads.php';
+} else {
+	$xmlurl = 'http://' . $xmlhost . '/chunks/downloads.php';
+}
+
+$xml = simplexml_load_file($xmlurl);
 
 if ($xml === false) echo '<!-- Failed to load XML --><h2><span style="color:#999">Load Error:</span> Failed to list Downloads.</h2>';
 
