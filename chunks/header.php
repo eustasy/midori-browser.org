ca2a4c495497f7610594a059797fa8c8a647954e
lewisgoddard <lewisgoddard@eustasy.co.uk>
2013-08-15 15:33:17 +0100

    Migrate all Donation boxes to function

--- chunks/header.php
+++ chunks/header.php
@@ -66,55 +66,7 @@
 			<script>$(".squishy").squishy();</script>
 		</div>
 		<div class="col span_1_of_6 donate">
-			<?php if ($Geo  == 'EU' || $Geo  == 'AD' || $Geo  == 'AT' || $Geo  == 'BE' || $Geo  == 'CY' || $Geo  == 'EE' || $Geo  == 'FI' || $Geo  == 'FR' || $Geo  == 'DE' || $Geo  == 'GR' || $Geo  == 'IE' || $Geo  == 'IT' || $Geo  == 'XK' || $Geo  == 'LU' || $Geo  == 'MT' || $Geo  == 'MC' || $Geo  == 'ME' || $Geo  == 'NL' || $Geo  == 'PT' || $Geo  == 'SM' || $Geo  == 'SK' || $Geo  == 'SI' || $Geo  == 'ES' || $Geo  == 'VA') { // EUR
-			echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="eur">
-				<input type="hidden" name="lc" value="US">
-				<input type="hidden" name="currency_code" value="EUR">';
-		} elseif ($Geo  == 'GG' || $Geo  == 'IM' || $Geo  == 'JE' || $Geo  == 'GS' || $Geo  == 'GB') { // GBP
-			echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="gbp">
-				<input type="hidden" name="lc" value="GB">
-				<input type="hidden" name="currency_code" value="GBP">';
-		} elseif ($Geo  == 'AU' || $Geo  == 'CC' || $Geo  == 'KI' || $Geo  == 'NR' || $Geo  == 'TV') { // AUD
-			echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="aud">
-				<input type="hidden" name="lc" value="AU">
-				<input type="hidden" name="currency_code" value="AUD">';
-		} elseif ($Geo  == 'CA') { // CAD
-			echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="cad">
-				<input type="hidden" name="lc" value="CA">
-				<input type="hidden" name="currency_code" value="CAD">';
-		} elseif ($Geo  == 'SE') { // SEK
-			echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="sek">
-				<input type="hidden" name="lc" value="SE">
-				<input type="hidden" name="currency_code" value="SEK">';
-		} else { // USD
-			echo '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="usd">
-				<input type="hidden" name="lc" value="US">
-				<input type="hidden" name="currency_code" value="USD">';
-		}
-
-	echo '
-				<input type="hidden" name="cmd" value="_xclick">
-				<input type="hidden" name="button_subtype" value="services">
-				<input type="hidden" name="no_shipping" value="1">
-				<input type="hidden" name="rm" value="1">
-
-				<input type="hidden" name="no_note" value="0">
-				<input type="hidden" name="cn" value="Add a note.">
-
-				<input type="hidden" name="business" value="christian@twotoasts.de">
-				<input type="hidden" name="item_name" value="Midori Browser Donation">
-				<input type="hidden" name="return" value="<?php echo $home; ?>/thanks">
-				<input type="hidden" name="cancel_return" value="<?php echo $home; ?>/ohwell">
-
-				<input type="hidden" name="bn" value="PP-BuyNowBF:btn_paynowCC_LG.gif:NonHosted">
-				<img alt="PayPal Payment Tracking" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" style="width:1px;height:1;border:none;">
-
-				<label for="amount">Donation Amount</label>
-				<input class="amount" name="amount" type="text" id="amount" maxlength="5" value="5.00">
-
-				<input class="submit" name="submit" type="submit" value="Donate">
-
-			</form>'; ?>
+			<?php donate($Geo); ?>
 			<p><a href="<?php echo $home; ?>/donate/">Other Currencies</a></p>
 		</div>
 	</header>
--- donate/index.php
+++ donate/index.php
@@ -19,195 +19,31 @@
 
 <div class="section group">
 	<div class="col span_1_of_3">
-
 		<h4>USD $</h4>
-
-		<form class="donate" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="usd">
-
-			<input type="hidden" name="cmd" value="_xclick">
-			<input type="hidden" name="lc" value="US">
-			<input type="hidden" name="currency_code" value="USD">
-			<input type="hidden" name="button_subtype" value="services">
-			<input type="hidden" name="no_shipping" value="1">
-			<input type="hidden" name="rm" value="1">
-
-			<input type="hidden" name="no_note" value="0">
-			<input type="hidden" name="cn" value="Add a note.">
-
-			<input type="hidden" name="business" value="christian@twotoasts.de">
-			<input type="hidden" name="item_name" value="Midori Browser Donation">
-			<input type="hidden" name="return" value="<?php echo $home; ?>/thanks">
-			<input type="hidden" name="cancel_return" value="<?php echo $home; ?>/ohwell">
-
-			<input type="hidden" name="bn" value="PP-BuyNowBF:btn_paynowCC_LG.gif:NonHosted">
-			<img alt="PayPal Payment Tracking" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" style="width:1px;height:1;border:none;">
-
-			<input class="amount" name="amount" type="text"  maxlength="5" value="5.00">
-
-			<input class="submit" name="submit" type="submit"  value="Donate">
-
-		</form>
-
+		<?php donate('US'); ?>
 	</div>
 	<div class="col span_1_of_3">
-
 		<h4>EUR &euro;</h4>
-
-		<form class="donate" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="eur">
-
-			<input type="hidden" name="cmd" value="_xclick">
-			<input type="hidden" name="lc" value="US">
-			<input type="hidden" name="currency_code" value="EUR">
-			<input type="hidden" name="button_subtype" value="services">
-			<input type="hidden" name="no_shipping" value="1">
-			<input type="hidden" name="rm" value="1">
-
-			<input type="hidden" name="no_note" value="0">
-			<input type="hidden" name="cn" value="Add a note.">
-
-			<input type="hidden" name="business" value="christian@twotoasts.de">
-			<input type="hidden" name="item_name" value="Midori Browser Donation">
-			<input type="hidden" name="return" value="<?php echo $home; ?>/thanks">
-			<input type="hidden" name="cancel_return" value="<?php echo $home; ?>/ohwell">
-
-			<input type="hidden" name="bn" value="PP-BuyNowBF:btn_paynowCC_LG.gif:NonHosted">
-			<img alt="PayPal Payment Tracking" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" style="width:1px;height:1;border:none;">
-
-			<input class="amount" name="amount" type="text"  maxlength="5" value="5.00">
-
-			<input class="submit" name="submit" type="submit"  value="Donate">
-
-		</form>
-
+		<?php donate('EU'); ?>
 	</div>
 	<div class="col span_1_of_3">
-
 		<h4>GBP &pound;</h4>
-
-		<form class="donate" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="gbp">
-
-			<input type="hidden" name="cmd" value="_xclick">
-			<input type="hidden" name="lc" value="GB">
-			<input type="hidden" name="currency_code" value="GBP">
-			<input type="hidden" name="button_subtype" value="services">
-			<input type="hidden" name="no_shipping" value="1">
-			<input type="hidden" name="rm" value="1">
-
-			<input type="hidden" name="no_note" value="0">
-			<input type="hidden" name="cn" value="Add a note.">
-
-			<input type="hidden" name="business" value="christian@twotoasts.de">
-			<input type="hidden" name="item_name" value="Midori Browser Donation">
-			<input type="hidden" name="return" value="<?php echo $home; ?>/thanks">
-			<input type="hidden" name="cancel_return" value="<?php echo $home; ?>/ohwell">
-
-			<input type="hidden" name="bn" value="PP-BuyNowBF:btn_paynowCC_LG.gif:NonHosted">
-			<img alt="PayPal Payment Tracking" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" style="width:1px;height:1;border:none;">
-
-			<input class="amount" name="amount" type="text"  maxlength="5" value="5.00">
-
-			<input class="submit" name="submit" type="submit"  value="Donate">
-
-		</form>
-
+		<?php donate('GB'); ?>
 	</div>
 </div>
 <div class="section group">
 	<div class="col span_1_of_3">
-
 		<h4>CAD $</h4>
-
-		<form class="donate" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="cad">
-
-			<input type="hidden" name="cmd" value="_xclick">
-			<input type="hidden" name="lc" value="CA">
-			<input type="hidden" name="currency_code" value="CAD">
-			<input type="hidden" name="button_subtype" value="services">
-			<input type="hidden" name="no_shipping" value="1">
-			<input type="hidden" name="rm" value="1">
-
-			<input type="hidden" name="no_note" value="0">
-			<input type="hidden" name="cn" value="Add a note.">
-
-			<input type="hidden" name="business" value="christian@twotoasts.de">
-			<input type="hidden" name="item_name" value="Midori Browser Donation">
-			<input type="hidden" name="return" value="<?php echo $home; ?>/thanks">
-			<input type="hidden" name="cancel_return" value="<?php echo $home; ?>/ohwell">
-
-			<input type="hidden" name="bn" value="PP-BuyNowBF:btn_paynowCC_LG.gif:NonHosted">
-			<img alt="PayPal Payment Tracking" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" style="width:1px;height:1;border:none;">
-
-			<input class="amount" name="amount" type="text"  maxlength="5" value="5.00">
-
-			<input class="submit" name="submit" type="submit"  value="Donate">
-
-		</form>
-
+		<?php donate('CA'); ?>
 	</div>
 	<div class="col span_1_of_3">
-
 		<h4>AUD $</h4>
-
-		<form class="donate" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="aud">
-
-			<input type="hidden" name="cmd" value="_xclick">
-			<input type="hidden" name="lc" value="AU">
-			<input type="hidden" name="currency_code" value="AUD">
-			<input type="hidden" name="button_subtype" value="services">
-			<input type="hidden" name="no_shipping" value="1">
-			<input type="hidden" name="rm" value="1">
-
-			<input type="hidden" name="no_note" value="0">
-			<input type="hidden" name="cn" value="Add a note.">
-
-			<input type="hidden" name="business" value="christian@twotoasts.de">
-			<input type="hidden" name="item_name" value="Midori Browser Donation">
-			<input type="hidden" name="return" value="<?php echo $home; ?>/thanks">
-			<input type="hidden" name="cancel_return" value="<?php echo $home; ?>/ohwell">
-
-			<input type="hidden" name="bn" value="PP-BuyNowBF:btn_paynowCC_LG.gif:NonHosted">
-			<img alt="PayPal Payment Tracking" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" style="width:1px;height:1;border:none;">
-
-			<input class="amount" name="amount" type="text"  maxlength="5" value="5.00">
-
-			<input class="submit" name="submit" type="submit"  value="Donate">
-
-		</form>
-
+		<?php donate('AU'); ?>
 	</div>
-
 	<div class="col span_1_of_3">
-
 		<h4>SEK kr</h4>
-
-		<form class="donate" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="sek">
-
-			<input type="hidden" name="cmd" value="_xclick">
-			<input type="hidden" name="lc" value="SE">
-			<input type="hidden" name="currency_code" value="SEK">
-			<input type="hidden" name="button_subtype" value="services">
-			<input type="hidden" name="no_shipping" value="1">
-			<input type="hidden" name="rm" value="1">
-
-			<input type="hidden" name="no_note" value="0">
-			<input type="hidden" name="cn" value="Add a note.">
-
-			<input type="hidden" name="business" value="christian@twotoasts.de">
-			<input type="hidden" name="item_name" value="Midori Browser Donation">
-			<input type="hidden" name="return" value="<?php echo $home; ?>/thanks">
-			<input type="hidden" name="cancel_return" value="<?php echo $home; ?>/ohwell">
-
-			<input type="hidden" name="bn" value="PP-BuyNowBF:btn_paynowCC_LG.gif:NonHosted">
-			<img alt="PayPal Payment Tracking" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" style="width:1px;height:1;border:none;">
-
-			<input class="amount" name="amount" type="text"  maxlength="5" value="5.00">
-
-			<input class="submit" name="submit" type="submit"  value="Donate">
-
-		</form>
-
+		<?php donate('SE'); ?>
 	</div>
-
 </div>
 <div class="section group"></div>
 
