d56a3556aac88848d556958b4ed21c6035e448da
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-08-15 20:07:48 +0100

    Update process.php

--- chunks/process.php
+++ chunks/process.php
@@ -8,12 +8,8 @@ $xml = simplexml_load_file($url);
 
 if ($xml === false) echo '<!-- Failed to load XML --><h2><span style="color:#999">Load Error:</span> Failed to list Downloads.</h2>';
 
-// foreach ($xml->download as $element) {
-// 	foreach($element as $key => $val) {
-// 		echo "{$key}: {$val}";
-// 		echo '<br>';
-// 	}
-// 	echo '<br>';
-// }
+foreach ($xml->download as $download) {
+  echo $download->extension;
+}
 
 ?>
