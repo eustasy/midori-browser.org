e5f3cd6516829f9868dc00377e15e9e049acd7f8
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-11 16:37:12 +0000

    Upgrade Fedora Download Page

--- download/fedora/index.php
+++ download/fedora/index.php
@@ -5,10 +5,34 @@
 <?php require '../../chunks/header.php'; ?>
 
 <div class="section group">
-	<div class="col span_3_of_4">
-		<div class="bubble standard">
-			<h2>Download Midori for Fedora</h2>
-			<div class="section group">
+        <div class="col span_1_of_6"><br></div>
+        <div class="col span_4_of_6">
+                <div class="bubble system fedora">
+                        <h2>Download Midori for Fedora</h2>
+                </div>
+        </div>
+        <div class="col span_1_of_6"><br></div>
+</div>
+
+<div class="section group">
+        <div class="col span_1_of_6"><br></div>
+        <div class="col span_4_of_6">
+                <div class="bubble standard">
+                        <h3>Install Direct</h3>
+                        <p class="important center">You can also install directly from the Fedora repositories with this command.</p>
+                        <input class="code" type="text" id="dynamic" value="sudo yum install midori" />
+                </div>
+        </div>
+        <div class="col span_1_of_6"><br></div>
+</div>
+
+<div class="section group">
+        <div class="col span_1_of_6"><br></div>
+        <div class="col span_4_of_6">
+                <div class="bubble standard">
+                        <h3>Alternative Downloads</h3>
+                        <p>These packages are available for download, and can be copied to machines without a connection to be installed. Note that they do not include any dependencies, and may not be updated with the rest of your system by the package manager.</p>
+                        <div class="section group">
 <?php
 
 include '../../chunks/process.php';
@@ -25,31 +49,21 @@ foreach ($xml->download as $download) {
 }
 
 ?>
-			</div>
-		</div>
-	</div>
-	<div class="col span_1_of_4">
-		<div class="bubble warning">
-			<h3>Install Direct</h3>
-			<p class="important">You can also install directly from the repositories with this command.</p>
-			<p>Note that the Fedora repositories may be out of date.</p>
-			<input type="text" id="dynamic" value="sudo yum install midori" />
-		</div>
-	</div>
+                        </div>
+                </div>
+        </div>
+        <div class="col span_1_of_6"><br></div>
 </div>
 
 <div class="section group">
-	<div class="col span_1_of_2">
-		<div class="bubble system fedora">
-			<h2>Fedora</h2>
-		</div>
-	</div>
-	<div class="col span_1_of_2">
-		<div class="bubble standard">
-			<h3 class="withimg64"><img src="<?php echo $home; ?>/images/feature-help.png" class="sixtyfour floatleft"> Help</h3>
-			<p>If you need to find out how to do something, you can check our <a href="<?php echo $home; ?>/faqs/">Frequently Asked Questions</a>.</p>
-		</div>
-	</div>
+        <div class="col span_1_of_6"><br></div>
+        <div class="col span_4_of_6">
+                <div class="bubble standard">
+                        <h3 class="withimg64"><img src="<?php echo $home; ?>/images/feature-help.png" class="sixtyfour floatleft"> Help</h3>
+                        <p>If you need to find out how to do something, you can check our <a href="<?php echo $home; ?>/faqs/">Frequently Asked Questions</a>.</p>
+                </div>
+        </div>
+        <div class="col span_1_of_6"><br></div>
 </div>
 
 <?php require '../../chunks/footer.php'; ?>
