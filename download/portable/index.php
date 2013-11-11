4ac45b8a96bf0f2bbb26dda98b769a2611425416
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-11 21:57:30 +0000

    Upgrade Midori Portable for Windows Download page

--- download/portable/index.php
+++ download/portable/index.php
@@ -5,62 +5,55 @@
 <?php require '../../chunks/header.php'; ?>
 
 <div class="section group">
-	<div class="col span_3_of_5">
-		<div class="bubble standard equalize">
-			<h2>Download Midori Portable<h2>
-			<h3>for Windows</h3>
-			<div class="section group">
+        <div class="col span_1_of_6"><br></div>
+        <div class="col span_4_of_6">
+                <div class="bubble system portable">
+                        <h2>Download Midori Portable for Windows</h2>
+                </div>
+        </div>
+        <div class="col span_1_of_6"><br></div>
+</div>
+
+<div class="section group">
+        <div class="col span_1_of_6"><br></div>
+        <div class="col span_4_of_6">
+                <div class="bubble standard">
+                	<div class="section group">
 <?php
 
 include '../../chunks/process.php';
 
 foreach ($xml->download as $download) {
 	if ($download->check == 'midori') {
-		if ($download->extension == '.7z') {
-			echo '<div class="col span_1_of_2">';
+		if ($download->extension == '.7z' || $download->extension == '.zip') {
 			$package = ltrim($download->extension, '.');
-			echo '<h3><a href="' . $download->location . '#!sha1!' . $download->sum . '">' . $package . '</a></h3>';
 			$size = $download->size/1048576;
-			echo '<h6>' . $download->version . ' &nbsp;&middot;&nbsp; ' . round($size, 1) . ' MB</h6>';
-			echo '</div>';
-		} elseif ($download->extension == '.zip') {
-			echo '<div class="col span_1_of_2">';
-			$package = ltrim($download->extension, '.');
-			echo '<h3><a href="' . $download->location . '#!sha1!' . $download->sum . '">' . $package . '</a></h3>';
-			$size = $download->size/1048576;
-			echo '<h6>' . $download->version . ' &nbsp;&middot;&nbsp; ' . round($size, 1) . ' MB</h6>';
-			echo '</div>';
+			echo '
+				<div class="col span_1_of_2">
+					<h3><a href="' . $download->location . '#!sha1!' . $download->sum . '">' . $package . '</a></h3>
+					<p class="center">' . $download->version . ' &nbsp;&middot;&nbsp; ' . round($size, 1) . ' MB</p>
+				</div>';
 		}
 	}
 }
 
 ?>
 			</div>
-			<h6><a href="http://www.7-zip.org/">7Zip</a> is a free, open-source archiving utility.</h6>
-		</div>
-	</div>
-	<div class="col span_2_of_5">
-		<div class="bubble standard equalize">
-			<h3>Usage</h3>
-			<p class="important">Midori Portable should work on all Windows systems, you simply need to extract the downloaded archive to a USB drive or folder on your computer, than run midori.exe</p>
-			<p>You can move the midori folder, copy it across computers, and run Midori from USB drives without having to install it.</p>
-		</div>
-	</div>
+			<p class="center"><a href="http://www.7-zip.org/">7Zip</a> is a free, open-source archiving utility.</p>
+                </div>
+        </div>
+        <div class="col span_1_of_6"><br></div>
 </div>
 
-
 <div class="section group">
-	<div class="col span_3_of_5">
-		<div class="bubble system portable equalize">
-			<h2>Portable</h2>
-		</div>
-	</div>
-	<div class="col span_2_of_5">
-		<div class="bubble standard equalize">
-			<h3 class="withimg64"><img src="<?php echo $home; ?>/images/feature-help.png" class="sixtyfour floatleft">Help</h3>
-			<p>If you need to find out how to do something, you can check our <a href="<?php echo $home; ?>/faqs/">Frequently Asked Questions</a>.</p>
-		</div>
-	</div>
+        <div class="col span_1_of_6"><br></div>
+        <div class="col span_4_of_6">
+                <div class="bubble standard">
+                        <h3 class="withimg64"><img src="<?php echo $home; ?>/images/feature-help.png" class="sixtyfour floatleft"> Help</h3>
+                        <p>If you need to find out how to do something, you can check our <a href="<?php echo $home; ?>/faqs/">Frequently Asked Questions</a>.</p>
+                </div>
+        </div>
+        <div class="col span_1_of_6"><br></div>
 </div>
 
 <?php require '../../chunks/footer.php'; ?>
