fc46c54f502554e7212c22d7360266cca98d16ba
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-11 16:19:01 +0000

    Update elementary Download

--- download/elementary/index.php
+++ download/elementary/index.php
@@ -4,32 +4,36 @@
 
 	<?php require '../../chunks/header.php'; ?>
 
-
+<div class="section group">
+	<div class="col span_1_of_6"><br></div>
+	<div class="col span_4_of_6">
+		<div class="bubble system elementary">
+			<h2 class="withimg64"><img src="<?php echo $home; ?>/images/action-love.png" class="sixtyfour floatleft"> Already installed!</h2>
+		</div>
+	</div>
+	<div class="col span_1_of_6"><br></div>
+</div>
 
 <div class="section group">
-	<div class="col span_1_of_1">
+	<div class="col span_1_of_6"><br></div>
+	<div class="col span_4_of_6">
 		<div class="bubble information">
-			<h2><img src="<?php echo $home; ?>/images/action-love.png" class="sixtyfour floatleft"> Already installed!</h2>
+			<h3>Already installed!</h3>
 			<h5>Midori comes pre-installed as the default browser in elementary os, you just need to click the icon down the bottom, or search for it in applications.</h5>
 		</div>
 	</div>
+	<div class="col span_1_of_6"><br></div>
 </div>
-
-
 
 <div class="section group">
-	<div class="col span_1_of_2">
-		<div class="bubble system elementary">
-			<h2>elementary</h2>
-		</div>
-	</div><div class="col span_1_of_2">
-		<div class="bubble standard">
-			<h3 class="withimg64"><img src="<?php echo $home; ?>/images/feature-help.png" class="sixtyfour floatleft">Help</h3>
-			<p>If you need to find out how to do something, you can check our <a href="<?php echo $home; ?>/faqs/">Frequently Asked Questions</a>, or try the <a href="https://plus.google.com/communities/104613975513761463450">Google Plus Community</a>.</p>
-		</div>
-	</div>
+        <div class="col span_1_of_6"><br></div>
+        <div class="col span_4_of_6">
+                <div class="bubble standard">
+                        <h3 class="withimg64"><img src="<?php echo $home; ?>/images/feature-help.png" class="sixtyfour floatleft"> Help</h3>
+                        <p>If you need to find out how to do something, you can check our <a href="<?php echo $home; ?>/faqs/">Frequently Asked Questions</a>, or try the <a href="https://plus.google.com/communities/104613975513761463450">Google Plus Community</a>.</p>
+                </div>
+        </div>
+        <div class="col span_1_of_6"><br></div>
 </div>
-
-
 
 <?php require '../../chunks/footer.php'; ?>
