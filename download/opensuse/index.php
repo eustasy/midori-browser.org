488014219498871d236b3c72c68e8bff4b1f4917
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-11 20:43:16 +0000

    Upgrade Suse Download page

--- download/opensuse/index.php
+++ download/opensuse/index.php
@@ -5,51 +5,69 @@
 <?php require '../../chunks/header.php'; ?>
 
 <div class="section group">
-	<div class="col span_3_of_4">
-		<div class="bubble standard">
-			<h2>Download Midori for Suse</h2>
-			<div class="section group">
+        <div class="col span_1_of_6"><br></div>
+        <div class="col span_4_of_6">
+                <div class="bubble system opensuse">
+                        <h2>Download Midori for Suse</h2>
+                </div>
+        </div>
+        <div class="col span_1_of_6"><br></div>
+</div>
+
+<div class="section group">
+        <div class="col span_1_of_6"><br></div>
+        <div class="col span_4_of_6">
+                <div class="bubble standard">
+                        <h3>Install Direct</h3>
+                        <p class="important">You can also install directly from the Suse repositories with this command. Note that these repositories are known to be outdated, and to get the latest version of Midori you should either enable the Cauldron repository or download the appropriate package from the alternative downloads section below.</p>
+                        <input class="code" type="text" id="dynamic" value="sudo yum install midori" />
+                </div>
+        </div>
+        <div class="col span_1_of_6"><br></div>
+</div>
+
+<div class="section group">
+        <div class="col span_1_of_6"><br></div>
+        <div class="col span_4_of_6">
+                <div class="bubble standard">
+                        <h3>Alternative Downloads</h3>
+                        <p>These packages are available for download, and can be copied to machines without a connection to be installed. Note that they do not include any dependencies, and may not be updated with the rest of your system by the package manager.</p>
+                        <div class="section group">
 <?php
 
 include '../../chunks/process.php';
 
 foreach ($xml->download as $download) {
-	if ($download->extension == '.rpm') {
-		echo '<div class="col span_1_of_3">';
-		echo '<h3><a href="' . $download->location . '#!sha1!' . $download->sum . '">' . $download->architecture . '</a></h3>';
-		$size = $download->size/1048576;
-		$package = ltrim($download->extension, '.');
-		echo '<h6>' . $package . ' &nbsp;&middot;&nbsp; ' . $download->version . ' &nbsp;&middot;&nbsp; ' . round($size, 1) . ' MB</h6>';
-		echo '</div>';
-	}
+        if ($download->extension == '.rpm') {
+                if ($download->architecture == 'i586' || $download->architecture == 'x86-64') {
+                        echo '
+                                <div class="col span_1_of_2">
+                                        <h3><a href="' . $download->location . '#!sha1!' . $download->sum . '">' . $download->architecture . '</a></h3>';
+                        $size = $download->size/1048576;
+                        $package = ltrim($download->extension, '.');
+                        echo '
+                                        <p class="center">' . $package . ' &nbsp;&middot;&nbsp; ' . $download->version . ' &nbsp;&middot;&nbsp; ' . round($size, 1) . ' MB</p>
+                                </div>';
+                }
+        }
 }
 
 ?>
-			</div>
-		</div>
-	</div>
-	<div class="col span_1_of_4">
-		<div class="bubble warning">
-			<h3>Install Direct</h3>
-			<p class="important">You can also install directly from the repositories with this command.</p>
-			<p>Note that the Suse repositories may be out of date.</p>
-			<input type="text" id="dynamic" value="sudo yum install midori" />
-		</div>
-	</div>
+                        </div>
+                </div>
+        </div>
+        <div class="col span_1_of_6"><br></div>
 </div>
 
 <div class="section group">
-	<div class="col span_1_of_2">
-		<div class="bubble system opensuse">
-			<h2>Suse</h2>
-		</div>
-	</div>
-	<div class="col span_1_of_2">
-		<div class="bubble standard">
-			<h3 class="withimg64"><img src="<?php echo $home; ?>/images/feature-help.png" class="sixtyfour floatleft">Help</h3>
-			<p>If you need to find out how to do something, you can check our <a href="<?php echo $home; ?>/faqs/">Frequently Asked Questions</a>.</p>
-		</div>
-	</div>
+        <div class="col span_1_of_6"><br></div>
+        <div class="col span_4_of_6">
+                <div class="bubble standard">
+                        <h3 class="withimg64"><img src="<?php echo $home; ?>/images/feature-help.png" class="sixtyfour floatleft"> Help</h3>
+                        <p>If you need to find out how to do something, you can check our <a href="<?php echo $home; ?>/faqs/">Frequently Asked Questions</a>.</p>
+                </div>
+        </div>
+        <div class="col span_1_of_6"><br></div>
 </div>
 
 <?php require '../../chunks/footer.php'; ?>
