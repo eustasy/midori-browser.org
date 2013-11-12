db321795be24716c4a21999ba0a0c8fbe63af008
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-12 15:16:48 +0000

    Add Modernizr to Fix IE <9

--- chunks/header.php
+++ chunks/header.php
@@ -11,6 +11,8 @@
 	<link rel="stylesheet" media="all" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700|Droid+Sans+Mono|Raleway:200,300,400">
 	<link rel="stylesheet" media="all" href="<?php echo $home; ?>/chunks/style.css">
 
+	<script src="<?php echo $home; ?>/chunks/modernizr.min.js"></script>
+
 	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 		ga('create', 'UA-28932148-9', 'midoribrowser.org');
