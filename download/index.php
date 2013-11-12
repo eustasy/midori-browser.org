3b08f7e09bdca7e1825e879400b62bd4ce2198e2
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-12 11:31:18 +0000

    Update Download Headers and Autodetect

--- download/index.php
+++ download/index.php
@@ -4,12 +4,6 @@
 
 	<?php require '../chunks/header.php'; ?>
 
-
-
-
-
-
-
 <?php
 
 require '../chunks/detect-two.php';
@@ -32,7 +26,7 @@ foreach ($xml->download as $download) {
 		echo '
 		<a href="' . $download->location . '#!sha1!' . $download->sum . '" title="Download Midori for Windows.">
 			<div class="bubble system windows">
-				<h2>Windows</h2>';
+				<h3>Windows</h3>';
 		echo '
 				<h6 class="right">' . $package . ' &nbsp;&middot;&nbsp; ' . $download->version . ' &nbsp;&middot;&nbsp; ' . round($size, 1) . ' MB</h6>';
 		echo '
@@ -46,7 +40,7 @@ foreach ($xml->download as $download) {
 	<div class="col span_1_of_2">
 		<a href="<?php echo $home; ?>/download/portable/" title="Download Portable Midori.">
 			<div class="bubble system portable">
-				<h2>Portable</h2>
+				<h3>Portable</h3>
 <?php
 
 echo '<h6 class="right">';
@@ -115,14 +109,14 @@ echo ' MB</h6>';
 	<div class="col span_1_of_2">
 		<a href="<?php echo $home; ?>/download/elementary/" title="Download Midori for elementary os.">
 			<div class="bubble system elementary">
-				<h2>elementary</h2>
+				<h3>elementary</h3>
 			</div>
 		</a>
 	</div>
 	<div class="col span_1_of_2">
 		<a href="<?php echo $home; ?>/download/ubuntu/" title="Download Midori for Ubuntu.">
 			<div class="bubble system ubuntu">
-				<h2>Ubuntu</h2>
+				<h3>Ubuntu</h3>
 			</div>
 		</a>
 	</div>
@@ -131,21 +125,21 @@ echo ' MB</h6>';
 	<div class="col span_1_of_3">
 		<a href="<?php echo $home; ?>/download/fedora/" title="Download Midori for Fedora.">
 			<div class="bubble system fedora">
-				<h3>Fedora</h3>
+				<h4>Fedora</h4>
 			</div>
 		</a>
 	</div>
 	<div class="col span_1_of_3">
 		<a href="<?php echo $home; ?>/download/debian/" title="Download Midori for Debian.">
 			<div class="bubble system debian">
-				<h3>Debian</h3>
+				<h4>Debian</h4>
 			</div>
 		</a>
 	</div>
 	<div class="col span_1_of_3">
 		<a href="<?php echo $home; ?>/download/mint/" title="Download Midori for Mint.">
 			<div class="bubble system mint">
-				<h3>Mint</h3>
+				<h4>Mint</h4>
 			</div>
 		</a>
 	</div>
@@ -154,28 +148,28 @@ echo ' MB</h6>';
 	<div class="col span_1_of_4">
 		<a href="<?php echo $home; ?>/download/mageia/" title="Download Midori for Mageia.">
 			<div class="bubble system mageia">
-				<h3>Mageia</h3>
+				<h4>Mageia</h4>
 			</div>
 		</a>
 	</div>
 	<div class="col span_1_of_4">
 		<a href="<?php echo $home; ?>/download/arch/" title="Download Midori for Arch.">
 			<div class="bubble system arch">
-				<h3>Arch</h3>
+				<h4>Arch</h4>
 			</div>
 		</a>
 	</div>
 	<div class="col span_1_of_4">
 		<a href="<?php echo $home; ?>/download/opensuse/" title="Download Midori for openSuse.">
 			<div class="bubble system opensuse">
-				<h3>Suse</h3>
+				<h4>Suse</h4>
 			</div>
 		</a>
 	</div>
 	<div class="col span_1_of_4">
 		<a href="<?php echo $home; ?>/download/source/" title="Download the Source of Midori.">
 			<div class="bubble system source">
-				<h3>Source</h3>
+				<h4>Source</h4>
 			</div>
 		</a>
 	</div>
@@ -190,7 +184,7 @@ echo ' MB</h6>';
 <div class="section group">
 	<div class="col span_2_of_4">
 		<div class="bubble warning">
-			<h2>Warning!</h2>
+			<h3>Warning!</h3>
 			<p>You appear to be using Mac OS X, and this application is not compatible with that system.</p>
 			<p class="right">Continue at your peril.</p>
 		</div>
@@ -205,7 +199,7 @@ echo ' MB</h6>';
 <div class="section group">
 	<div class="col span_2_of_4">
 		<div class="bubble warning">
-			<h2>Warning!</h2>
+			<h3>Warning!</h3>
 			<p>We couldn't detect your Operating System, and as such cannot guarantee this applications compatibility.</p>
 			<p class="right">Continue at your peril.</p>
 		</div>
