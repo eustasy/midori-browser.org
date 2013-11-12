d5fa7dc1cf0dc979476d0e282be671a6f4cbf873
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-12 11:34:17 +0000

    Update fallback.php

--- chunks/fallback.php
+++ chunks/fallback.php
@@ -1,14 +1,14 @@
 	<div class="col span_1_of_2">
 		<a href="<?php echo $home; ?>/download/elementary/" title="Download Midori for elementary.">
 			<div class="bubble system elementary">
-				<h2>elementary</h2>
+				<h3>elementary</h3>
 			</div>
 		</a>
 	</div>
 	<div class="col span_1_of_2">
 		<a href="<?php echo $home; ?>/download/ubuntu/" title="Download Midori for Ubuntu.">
 			<div class="bubble system ubuntu">
-				<h2>Ubuntu</h2>
+				<h3>Ubuntu</h3>
 			</div>
 		</a>
 	</div>
@@ -17,21 +17,21 @@
 	<div class="col span_1_of_3">
 		<a href="<?php echo $home; ?>/download/fedora/" title="Download Midori for Fedora.">
 			<div class="bubble system fedora">
-				<h3>Fedora</h3>
+				<h4>Fedora</h4>
 			</div>
 		</a>
 	</div>
 	<div class="col span_1_of_3">
 		<a href="<?php echo $home; ?>/download/debian/" title="Download Midori for Debian.">
 			<div class="bubble system debian">
-				<h3>Debian</h3>
+				<h4>Debian</h4>
 			</div>
 		</a>
 	</div>
 	<div class="col span_1_of_3">
 		<a href="<?php echo $home; ?>/download/mint/" title="Download Midori for Mint.">
 			<div class="bubble system mint">
-				<h3>Mint</h3>
+				<h4>Mint</h4>
 			</div>
 		</a>
 	</div>
@@ -40,30 +40,30 @@
 	<div class="col span_1_of_4">
 		<a href="<?php echo $home; ?>/download/mageia/" title="Download Midori for Mageia.">
 			<div class="bubble system mageia">
-				<h3>Mageia</h3>
+				<h4>Mageia</h4>
 			</div>
 		</a>
 	</div>
 	<div class="col span_1_of_4">
 		<a href="<?php echo $home; ?>/download/arch/" title="Download Midori for Arch.">
 			<div class="bubble system arch">
-				<h3>Arch</h3>
+				<h4>Arch</h4>
 			</div>
 		</a>
 	</div>
 	<div class="col span_1_of_4">
 		<a href="<?php echo $home; ?>/download/opensuse/" title="Download Midori for openSuse.">
 			<div class="bubble system opensuse">
-				<h3>Suse</h3>
+				<h4>Suse</h4>
 			</div>
 		</a>
 	</div>
 	<div class="col span_1_of_4">
 		<a href="<?php echo $home; ?>/download/source/" title="Download the Source of Midori.">
 			<div class="bubble system source">
-				<h3>Source</h3>
+				<h4>Source</h4>
 			</div>
 		</a>
 	</div>
 </div>
-<p class="center"><a href="<?php echo $home; ?>/download/choose/" title="Other ways to get Midori / Get Midori for Other Systems.">Other ways to get Midori and Midori for Other Systems.</a></p>
+<p class="center"><a href="<?php echo $home; ?>/download/choose/" title="Other ways to get Midori / Midori for Other Systems">Other ways to get Midori / Midori for Other Systems</a></p>
