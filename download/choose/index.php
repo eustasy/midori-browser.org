9b176aa5c93f0fb6dde341c5538fea25216497af
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-12 11:22:40 +0000

    Upgrade Choose Download

--- download/choose/index.php
+++ download/choose/index.php
@@ -18,7 +18,7 @@ foreach ($xml->download as $download) {
 		echo '
 			<a href="' . $download->location . '#!sha1!' . $download->sum . '" title="Download Midori for Windows.">
 				<div class="bubble system windows">
-					<h3>Windows</h3>
+					<h2>Windows</h2>
 					<h6 class="right">' . $package . ' &nbsp;&middot;&nbsp; ' . $download->version . ' &nbsp;&middot;&nbsp; ' . round($size, 1) . ' MB</h6>
 				</div>
 			</a>';
@@ -26,14 +26,15 @@ foreach ($xml->download as $download) {
 }
 
 ?>
-	</div>
-	<div class="col span_1_of_2">
-		<a href="<?php echo $home; ?>/download/portable/" title="Download Portable Midori.">
-			<div class="bubble system portable">
-				<h2>Portable</h2>
+		</div>
+		<div class="col span_1_of_2">
+			<a href="<?php echo $home; ?>/download/portable/" title="Download Portable Midori.">
+				<div class="bubble system portable">
+					<h2>Portable</h2>
 <?php
 
-echo '<h6 class="right">';
+echo '
+					<h6 class="right">';
 
 foreach ($xml->download as $download) {
 	if ($download->extension == '.7z') {
@@ -82,79 +83,79 @@ foreach ($xml->download as $download) {
 echo ' MB</h6>';
 
 ?>
-			</div>
-		</a>
+				</div>
+			</a>
+		</div>
 	</div>
-</div>
-<div class="section group" id="second">
-	<div class="col span_1_of_2">
-		<a href="<?php echo $home; ?>/download/elementary/" title="Download Midori for elementary os.">
-			<div class="bubble system elementary">
-				<h2>elementary</h2>
-			</div>
-		</a>
+	<div class="section group" id="second">
+		<div class="col span_1_of_2">
+			<a href="<?php echo $home; ?>/download/elementary/" title="Download Midori for elementary os.">
+				<div class="bubble system elementary">
+					<h2>elementary</h2>
+				</div>
+			</a>
+		</div>
+		<div class="col span_1_of_2">
+			<a href="<?php echo $home; ?>/download/ubuntu/" title="Download Midori for Ubuntu.">
+				<div class="bubble system ubuntu">
+					<h2>Ubuntu</h2>
+				</div>
+			</a>
+		</div>
 	</div>
-	<div class="col span_1_of_2">
-		<a href="<?php echo $home; ?>/download/ubuntu/" title="Download Midori for Ubuntu.">
-			<div class="bubble system ubuntu">
-				<h2>Ubuntu</h2>
-			</div>
-		</a>
+	<div class="section group" id="third">
+		<div class="col span_1_of_3">
+			<a href="<?php echo $home; ?>/download/fedora/" title="Download Midori for Fedora.">
+				<div class="bubble system fedora">
+					<h3>Fedora</h3>
+				</div>
+			</a>
+		</div>
+		<div class="col span_1_of_3">
+			<a href="<?php echo $home; ?>/download/debian/" title="Download Midori for Debian.">
+				<div class="bubble system debian">
+					<h3>Debian</h3>
+				</div>
+			</a>
+		</div>
+		<div class="col span_1_of_3">
+			<a href="<?php echo $home; ?>/download/mint/" title="Download Midori for Mint.">
+				<div class="bubble system mint">
+					<h3>Mint</h3>
+				</div>
+			</a>
+		</div>
 	</div>
-</div>
-<div class="section group" id="third">
-	<div class="col span_1_of_3">
-		<a href="<?php echo $home; ?>/download/fedora/" title="Download Midori for Fedora.">
-			<div class="bubble system fedora">
-				<h3>Fedora</h3>
-			</div>
-		</a>
+	<div class="section group" id="third">
+		<div class="col span_1_of_4">
+			<a href="<?php echo $home; ?>/download/mageia/" title="Download Midori for Mageia.">
+				<div class="bubble system mageia">
+					<h3>Mageia</h3>
+				</div>
+			</a>
+		</div>
+		<div class="col span_1_of_4">
+			<a href="<?php echo $home; ?>/download/arch/" title="Download Midori for Arch.">
+				<div class="bubble system arch">
+					<h3>Arch</h3>
+				</div>
+			</a>
+		</div>
+		<div class="col span_1_of_4">
+			<a href="<?php echo $home; ?>/download/opensuse/" title="Download Midori for openSuse.">
+				<div class="bubble system opensuse">
+					<h3>Suse</h3>
+				</div>
+			</a>
+		</div>
+		<div class="col span_1_of_4">
+			<a href="<?php echo $home; ?>/download/source/" title="Download the Source of Midori.">
+				<div class="bubble system source">
+					<h3>Source</h3>
+				</div>
+			</a>
+		</div>
 	</div>
-	<div class="col span_1_of_3">
-		<a href="<?php echo $home; ?>/download/debian/" title="Download Midori for Debian.">
-			<div class="bubble system debian">
-				<h3>Debian</h3>
-			</div>
-		</a>
-	</div>
-	<div class="col span_1_of_3">
-		<a href="<?php echo $home; ?>/download/mint/" title="Download Midori for Mint.">
-			<div class="bubble system mint">
-				<h3>Mint</h3>
-			</div>
-		</a>
-	</div>
-</div>
-<div class="section group" id="third">
-	<div class="col span_1_of_4">
-		<a href="<?php echo $home; ?>/download/mageia/" title="Download Midori for Mageia.">
-			<div class="bubble system mageia">
-				<h3>Mageia</h3>
-			</div>
-		</a>
-	</div>
-	<div class="col span_1_of_4">
-		<a href="<?php echo $home; ?>/download/arch/" title="Download Midori for Arch.">
-			<div class="bubble system arch">
-				<h3>Arch</h3>
-			</div>
-		</a>
-	</div>
-	<div class="col span_1_of_4">
-		<a href="<?php echo $home; ?>/download/opensuse/" title="Download Midori for openSuse.">
-			<div class="bubble system opensuse">
-				<h3>Suse</h3>
-			</div>
-		</a>
-	</div>
-	<div class="col span_1_of_4">
-		<a href="<?php echo $home; ?>/download/source/" title="Download the Source of Midori.">
-			<div class="bubble system source">
-				<h3>Source</h3>
-			</div>
-		</a>
-	</div>
-</div>
 
 
 
