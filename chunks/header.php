d38d56e73b1ce952f71d982145fe4ce0bc7d2dcc
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-14 14:07:42 +0000

    Updated Header

--- chunks/header.php
+++ chunks/header.php
@@ -29,24 +29,6 @@
 <div id="skiptomain"><a href="#maincontent">skip to main content</a></div>
 
 
-
-<div id="headcontainer">
-	<header class="section group">
-		<div class="col span_1_of_6">
-			<img src="<?php echo $home; ?>/images/midori.png" alt="Midori Logo">
-		</div>
-		<div class="col span_1_of_3">
-			<a href="<?php echo $home; ?>/" title="Midori, a lightweight, fast, and free web browser."><h1 class="squishy">Midori</h1></a>
-			<a href="<?php echo $home; ?>/" title="Midori, a lightweight, fast, and free web browser."><p class="tag squishy">a lightweight, fast, and free web browser</p></a>
-		</div>
-		<div class="col span_1_of_6 donate">
-			<?php donate($Geo); ?>
-			<p><a href="<?php echo $home; ?>/donate/">Other Currencies</a></p>
-		</div>
-	</header>
-</div>
-
-
 <div class="navwrap">
 	<nav class="section group">
 		<a href="<?php echo $home; ?>/about/" title="About Midori" class="col span_1_of_6<?php if ( $location == '/about/' ) { echo ' current'; } ?>">About</a>
@@ -56,6 +38,14 @@
 		<a href="<?php echo $home; ?>/contribute/" title="Contribute to Midori"  class="col span_1_of_6<?php if ( $location == '/contribute/' ) { echo ' current'; } ?>">Contribute</a>
 		<a href="<?php echo $home; ?>/download/" title="Download Midori"  class="col span_1_of_6<?php if ( $location == '/download/' ) { echo ' current'; } ?> down">Download</a>
 	</nav>
+</div>
+
+
+<div id="headcontainer">
+	<header class="section group">
+		<h1><a href="<?php echo $home; ?>/" title="Midori, a lightweight, fast, and free web browser."><img src="<?php echo $home; ?>/images/midori.png" alt="Midori Logo">Midori</a></h1>
+		<a href="<?php echo $home; ?>/" title="Midori, a lightweight, fast, and free web browser."><p class="tag">A lightweight, fast, and free web browser.</p></a>
+	</header>
 </div>
 
 
