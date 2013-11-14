3143347d4a82f04966d0de749f681c936f8a617d
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-14 14:26:14 +0000

    GitPull

--- gitpull.php
+++ gitpull.php
@@ -19,7 +19,7 @@ if ( $auth === 'glaflzbhlaczddmuxurmlujmdqtgvoqzzbpljshtfrohsqzfbtmazkgnkmudqaiq
 	$output = nl2br( shell_exec('git reset --hard HEAD && git pull') ); // Capture the output.
 
 } else { // Or not.
-	$output = nl2br( shell_exec('git reset --hard HEAD') ); // Just tell us where we are
+	$output = nl2br( shell_exec('git reset --hard HEAD && git pull') ); // Just tell us where we are
 }
 
 // That is all.
