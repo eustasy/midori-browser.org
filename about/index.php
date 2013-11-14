cd43081c0d82b68011740632a0cd3e1174420a96
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-14 21:07:37 +0000

    Remove Bubbles from About Page

--- about/index.php
+++ about/index.php
@@ -9,40 +9,37 @@
 <h2>About</h2>
 
 <div class="section group">
-	<div class="col span_1_of_2">
-		<div class="bubble standard">
-			<h3>The Browser</h3>
-			<p><a href="<?php echo $home; ?>/">Midori</a> is a lightweight, fast, and free web browser. It aligns well with the Xfce philosophy of making the most out of available resources. It is also the default browser of the beautiful <a href="http://elementaryos.org/">elementary os</a>.</p>
-			<div class="section group">
-				<div class="col span_1_of_2">
-					<ul>
-						<li>Fast</li>
-						<li>Lightweight</li>
-						<li>Open-Source</li>
-					</ul>
-				</div>
-				<div class="col span_1_of_2">
-					<ul>
-						<li>GTK 2/3	</li>
-						<li>WebKit</li>
-						<li>Granite</li>
-					</ul>
-				</div>
+	<div class="col span_5_of_11">
+		<h3>The Browser</h3>
+		<p><a href="<?php echo $home; ?>/">Midori</a> is a lightweight, fast, and free web browser. It aligns well with the Xfce philosophy of making the most out of available resources. It is also the default browser of the beautiful <a href="http://elementaryos.org/">elementary os</a>.</p>
+		<div class="section group">
+			<div class="col span_1_of_2">
+				<ul>
+					<li>Fast</li>
+					<li>Lightweight</li>
+					<li>Open-Source</li>
+				</ul>
 			</div>
-			<h4>The License</h4>
-			<p>Midori is <a title="Midori PPA" href="https://launchpad.net/~midori/+archive/ppa">release</a> under the <a rel="license" href="http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html">GNU Lesser General Public License, version 2.1</a>, which makes it suitable for inclusion in both proprietary and free, open-source programs.</p>
-		</div>
+			<div class="col span_1_of_2">
+				<ul>
+					<li>GTK 2/3	</li>
+					<li>WebKit</li>
+					<li>Granite</li>
+				</ul>
+			</div>
+			</div>
+		<h4>The License</h4>
+		<p>Midori is <a title="Midori PPA" href="https://launchpad.net/~midori/+archive/ppa">release</a> under the <a rel="license" href="http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html">GNU Lesser General Public License, version 2.1</a>, which makes it suitable for inclusion in both proprietary and free, open-source programs.</p>
 	</div>
-	<div class="col span_1_of_2">
-		<div class="bubble standard">
-			<h3>The Developers</h3>
-			<div class="clear"><br></div>
-			<h4>Christian Dywan</h4>
-			<p>Christian likes to cook and eat Japanese. Add to that a passion for Chinese and Japanese tea. Christian enjoys hacking on Midori, working with elementary projects and QML. His development machine is an elementary Xfce hybrid based on Ubuntu.</p>
-			<div class="clear"><br></div>
-			<h4>Nancy Runge</h4>
-			<p>Nancy likes to cook and eat Japanese as well. She is a voluntary cat petter in a local animal shelter.</p>
-		</div>
+	<div class="col span_1_of_11"</div>
+	<div class="col span_5_of_11">
+		<h3>The Developers</h3>
+		<div class="clear"><br></div>
+		<h4>Christian Dywan</h4>
+		<p>Christian likes to cook and eat Japanese. Add to that a passion for Chinese and Japanese tea. Christian enjoys hacking on Midori, working with elementary projects and QML. His development machine is an elementary Xfce hybrid based on Ubuntu.</p>
+		<div class="clear"><br></div>
+		<h4>Nancy Runge</h4>
+		<p>Nancy likes to cook and eat Japanese as well. She is a voluntary cat petter in a local animal shelter.</p>
 	</div>
 </div>
 
