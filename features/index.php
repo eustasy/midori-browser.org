9849fb6554f8201cb5953f6c8d2a06b6a9d408d5
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-14 21:10:18 +0000

    Update index.php

--- features/index.php
+++ features/index.php
@@ -8,19 +8,19 @@
 
 <div class="section group">
 	<div class="col span_1_of_3">
-		<div class="bubble standard">
+		<div class="bubble">
 			<h3>Performance</h3>
 			<p><img src="<?php echo $home; ?>/images/feature-performance.png" class="sixtyfour floatleft">Midori is a lightweight, super-fast web browser, where speed behind a beautiful browser has always been an end goal.</p>
 		</div>
 	</div>
 	<div class="col span_1_of_3">
-		<div class="bubble standard">
+		<div class="bubble">
 			<h3>Simple</h3>
 			<p><img src="<?php echo $home; ?>/images/feature-view.png" class="sixtyfour floatleft">The simple, easy to use, big-button interface makes Midori not only easy to use, but also leaves more space for the site you want to see.</p>
 		</div>
 	</div>
 	<div class="col span_1_of_3">
-		<div class="bubble standard">
+		<div class="bubble">
 			<h3>Settings</h3>
 			<p><img src="<?php echo $home; ?>/images/feature-settings.png" class="sixtyfour floatleft">With simple but highly customizable settings, and defaults with privacy in mind, Midori really set a standard for browser simplicity.</p>
 		</div>
@@ -29,25 +29,25 @@
 
 <div class="section group">
 	<div class="col span_1_of_4">
-		<div class="bubble standard">
+		<div class="bubble">
 			<h4>Bookmarks</h4>
 			<p><img src="<?php echo $home; ?>/images/feature-bookmarks.png" class="fortyeight floatleft">Everyone loves bookmarks, and you can save sites to your hearts content. You can even create launchers and add things to your Speed Dial.</p>
 		</div>
 	</div>
 	<div class="col span_1_of_4">
-		<div class="bubble standard">
+		<div class="bubble">
 			<h4>RSS</h4>
 			<p><img src="<?php echo $home; ?>/images/feature-rss.png" class="fortyeight floatleft">RSS, Atom, and XML Feeds have become a staple of keeping up-to-date with the latest news. Midori can detect and add them to whichever client you choose.</p>
 		</div>
 	</div>
 	<div class="col span_1_of_4">
-		<div class="bubble standard">
+		<div class="bubble">
 			<h4>Spell Checker</h4>
 			<p><img src="<?php echo $home; ?>/images/feature-dictionary.png" class="fortyeight floatleft">You spelt necessary wrong again. At least that red squigly line knows is nice enough to tell you every time. I wonder if it knows how to spell supercalifragi...</p>
 		</div>
 	</div>
 	<div class="col span_1_of_4">
-		<div class="bubble standard">
+		<div class="bubble">
 			<h4>Download</h4>
 			<p><img src="<?php echo $home; ?>/images/feature-download.png" class="fortyeight floatleft">The speedy built-in downloader will fetch those songs for you in no time. Another album? Okay. A movie? No problem. Lets download the internet!</p>
 		</div>
