6f7628c8cdf769d86649d1b68d458ed5952027d8
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-01-19 16:17:12 +0000

    Added Changelog

---
+++ changelog/index.php
@@ -0,0 +1,15 @@
+<?php require '../chunks/doctype.php'; ?>
+
+<title>Changelog &nbsp;&middot;&nbsp; Midori &nbsp;&middot;&nbsp; A lightweight, fast, and free web browser.</title>
+
+<?php require '../chunks/header.php'; ?>
+
+<?php
+// What we're gonna get
+$url = 'http://wiki.xfce.org/_export/xhtml/midori/changelog';
+$edit = 'https://wiki.xfce.org/midori/changelog';
+
+include '../chunks/dokuimport.php';
+
+
+require '../chunks/footer.php'; ?>
