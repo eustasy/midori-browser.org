b684f7094d7f9c6cf088ee8ebc5cbec90e086f61
Lewis Goddard <lewisgoddard@eustasy.org>
2015-05-05 23:30:30 +0100

    Secure FAQ

--- faqs/index.php
+++ faqs/index.php
@@ -4,10 +4,7 @@
 
 <?php require '../chunks/header.php';
 
-// Enable Debuggin
-error_reporting(~0);
-ini_set('display_errors', 1);
 // Import this page
-dokuImport('http://wiki.xfce.org/_export/xhtml/midori/faq', 'https://wiki.xfce.org/midori/faq');
+dokuImport('https://wiki.xfce.org/_export/xhtml/midori/faq', 'https://wiki.xfce.org/midori/faq');
 
 require '../chunks/footer.php';
