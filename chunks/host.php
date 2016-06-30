2d49cfe64834f90edfc09e8a0b3c680bfc27296e
Lewis Goddard <lewisgoddard@eustasy.org>
2016-06-30 01:09:07 +0100

    Update logo, fix localtest.me, update jquery

--- chunks/host.php
+++ chunks/host.php
@@ -2,8 +2,8 @@
 
 $host = htmlspecialchars($_SERVER['HTTP_HOST'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
 
-if ($host === 'localhost') {
-	$home = 'http://localhost/midori-browser.org';
+if ($host === 'local.localtest.me') {
+	$home = 'http://local.localtest.me/midori-browser.org';
 	$location = htmlspecialchars($_SERVER['REQUEST_URI'], ENT_QUOTES | ENT_HTML5, 'UTF-8');
 	$location = str_replace('/midori-browser.org', '', $location);
 } else {
